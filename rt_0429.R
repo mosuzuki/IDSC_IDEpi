fe52c0668f4ac566a19df7821b2cce06d34c2a11
motoisuzuki <45912757+mosuzuki@users.noreply.github.com>
2020-05-05 01:00:29 +0900

    Update rt_0429.R

--- rt_0429.R
+++ rt_0429.R
@@ -2,27 +2,60 @@ library("incidence")
 library("ggplot2")
 library("R0")
 library("earlyR")
+library("mice")
 
-setwd("C:/xxxxx")
-cov <- read.csv("cov0428.csv")
 
-#Tokyo and surrounding prefectures
-#cov <- subset(cov, pref=="“Œ‹ž"|pref=="_“Þì"|pref=="é‹Ê"|pref=="ç—t")
+setwd("C:/Users/xxxxx/")
+cov <- read.csv("cov0504.csv")
 
-cov$ons <- as.Date(cov$ons,format="%m/%d/%y")
-cov$rep <- as.Date(cov$rep,format="%m/%d/%y")
+cov$ons <- as.Date(cov$ons,format="%m/%d/%y", origin="1970-01-01")
+cov$rep <- as.Date(cov$rep,format="%m/%d/%y", origin="1970-01-01")
 
-#simple imputation for date of onset as date of report - 7ds
-cov$ons[is.na(cov$ons)] <- cov$rep-7
+cov$del <- as.numeric(cov$rep-cov$ons)
 
-#estimated date of infection as date of onset - 5ds
-cov$inf <- cov$ons-5
+#simple imputation for date of onset
+cov.1 <- data.frame(cov$seq, cov$pref, cov$age, cov$sex, cov$import, cov$symp, cov$contact, cov$del)
+
+cov.1$cov.age[is.na(cov.1$cov.age)] <- 999
+cov.1$cov.import[is.na(cov.1$cov.import)] <- 999
+cov.1$cov.symp[is.na(cov.1$cov.symp)] <- 999
+cov.1$cov.contact[is.na(cov.1$cov.contact)] <- 999
+
+
+imp <- mice(cov.1, method = "norm.predict", m = 1)
+
+cov.imp <- complete(imp)
+cov.imp$cov.del[cov.imp$cov.del<0] <-0
+cov.imp$cov.del <- round(cov.imp$cov.del)
+
+cov.2 <- data.frame(cov.imp$cov.seq, cov.imp$cov.del)
+cov.2$seq <- cov.2$cov.imp.cov.seq
+cov.2$del <- cov.2$cov.imp.cov.del
+
+cov.3 <- merge(cov, cov.2, by = "seq", all = T) 
+cov.3$ons <- cov.3$rep - cov.3$del.y 
+
+cov <- cov.3
+
+#estimated date of infection
+n <- nrow(cov)
+lamda <- 5
+cov$inc <- rpois(n, lamda)
+
+cov$inf <- cov$ons-cov$inc
 
 cov$import[cov$import>2] <- 0
+
+###
+#Tokyo and surrounding prefectures
+#cov <- subset(cov, pref=="東京"|pref=="神奈川"|pref=="埼玉"|pref=="千葉")
+#cov <- subset(cov, pref=="北海道")
+###
+
 cov.imp <- subset(cov, cov$import==1)
 
-rep <- incidence(cov$rep, interval = 1)
-ons <- incidence(cov$ons, interval = 1)
+#daily incidence
+
 inf <- incidence(cov$inf, interval = 1)
 imp <- incidence(cov.imp$inf, interval = 1)
 
@@ -34,17 +67,17 @@ colnames(inc.imp) <- c("inf.dates","imp.counts")
 df.1 <- merge(inc.inf, inc.imp, by = "inf.dates", all = T) 
 df.1$imp.counts[is.na(df.1$imp.counts)] <- 0
 
-###
-#EID
-#GT <- generation.time(type="weibull", c(3.96,4.75))
+#effective reproduction number
+#Bi et al. LID 2020.
+GT <- generation.time("weibull", c(6.3, 4.2))
 #Nishiura
-GT <- generation.time("weibull", c(4.2, 2.9))
+#GT <- generation.time("weibull", c(4.2, 2.9))
 
-TD <- est.R0.TD(epid=df.1$inf.counts, GT, import = df.1$imp.counts, begin=18, end=110, nsim=2000)
+TD <- est.R0.TD(epid=df.1$inf.counts, GT, import = df.1$imp.counts, begin=18, end=114, nsim=2000)
 
 df <- data.frame(TD$R, TD$conf.int)
 
-df$inf.dates <- seq(as.Date("2020-01-20"), as.Date("2020-04-21"), "days")
+df$inf.dates <- seq(as.Date("2020-01-20"), as.Date("2020-04-25"), "days")
 
 df <- merge(inc.inf, df, by = "inf.dates", all = T) 
 
@@ -60,6 +93,7 @@ ggplot(df, aes(x=inf.dates, y=inf.counts))+
                                   "2020-04-01", "2020-04-15"))) +
   scale_y_continuous(sec.axis = sec_axis(~.*(1/100), name = "Effective reproduction number"))+
   geom_line(y=100, colour="black", linetype = "dashed")+
+  labs(title="Entire Japan: by M.Suzuki")+
   theme(legend.position ="none")
 
-####
+###
